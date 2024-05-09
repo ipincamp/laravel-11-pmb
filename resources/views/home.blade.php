de33cb027eeb6056c0302f2e54746cff5ad54a74
ipincamp <arif.lngsrn@gmail.com>
2024-05-09 10:05:30 +0700

    Add progress bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8 py-3 px-3">
+            <div class="col-md-10 py-3 px-3">
                 <div class="card">
                     <div class="card-header">{{ __('Dashboard') }}</div>
 
@@ -14,14 +14,17 @@
                             </div>
                         @endif
 
-                        {{ __('Progress Bar') }}
+                        {{ __('Progress') }}
+                        <div class="progress mt-1" role="progressbar" aria-label="Danger striped example" aria-valuenow="90" aria-valuemin="0" aria-valuemax="90">
+                            <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" style="width: 90%"></div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
 
         <div class="row justify-content-center">
-            <div class="col-md-8 py-3 px-3">
+            <div class="col-md-10 py-3 px-3">
                 <h1>Pengumuman Kelulusan</h1>
                 <span class="text-small">{{ __('Akan muncul jika semua progress selesai') }}</span>
                 <p>Berikut adalah hasil kelulusan Anda:</p>
