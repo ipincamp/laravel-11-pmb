5b5dacfc4639e613f82e92fbeac90bb0efddb9ff
ipincamp <arif.lngsrn@gmail.com>
2024-05-24 19:04:13 +0700

    refactor: Add relationship between DataPath and Mahasiswa models

--- app/Models/DataPath.php
+++ app/Models/DataPath.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DataPath extends Model
 {
@@ -11,4 +12,14 @@ class DataPath extends Model
         'path',
         'mahasiswa_id',
     ];
+
+    /**
+     * Get the mahasiswa that owns the DataPath
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function mahasiswa(): BelongsTo
+    {
+        return $this->belongsTo(DataMahasiswa::class, 'mahasiswa_id', 'id');
+    }
 }
