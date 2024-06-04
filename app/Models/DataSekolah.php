2b8d7cffc77f90b80e8616882becc89d95c53c3b
ipincamp <support@nur-arifin.my.id>
2024-06-04 16:12:08 +0700

    feat: Add DataSekolah model and migration

---
+++ app/Models/DataSekolah.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DataSekolah extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'nisn',
+        'nama',
+        'alamat',
+        'derajat',
+        'jurusan',
+        'mahasiswa_id',
+    ];
+}
