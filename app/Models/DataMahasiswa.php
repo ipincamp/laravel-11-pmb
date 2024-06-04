fa9149fa2a7b368350ec431babdea397a8b4f7f4
ipincamp <support@nur-arifin.my.id>
2024-06-04 16:07:30 +0700

    chore: Add DataMahasiswa model and migration

---
+++ app/Models/DataMahasiswa.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DataMahasiswa extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'tempat_lahir',
+        'tanggal_lahir',
+        'jenis_kelamin',
+        'agama',
+        'alamat',
+        'rt',
+        'rw',
+        'kelurahan',
+        'kecamatan',
+        'kabupaten',
+        'provinsi',
+        'no_hp',
+        'user_id',
+    ];
+}
---
+++ database/migrations/2024_06_04_160122_create_data_mahasiswas_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('data_mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->enum('jenis_kelamin', ['L', 'P']);
+            $table->enum('agama', ['ISLAM', 'KRISTEN', 'KATOLIK', 'HINDU', 'BUDHA', 'KONGHUCU']);
+            $table->string('alamat');
+            $table->integer('rt');
+            $table->integer('rw');
+            $table->string('kelurahan');
+            $table->string('kecamatan');
+            $table->string('kabupaten');
+            $table->string('provinsi');
+            $table->string('no_hp');
+            $table->foreignId('user_id')->constrained(
+                table: 'users',
+                indexName: 'user_id_fk',
+            )->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('data_mahasiswas');
+    }
+};
