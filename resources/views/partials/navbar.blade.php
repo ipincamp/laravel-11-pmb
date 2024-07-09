c58b7f4ffe92583a6f66566fab5e8314c09a01bb
ipincamp <support@nur-arifin.my.id>
2024-07-10 00:12:32 +0700

    chore: Update navbar logo source
    The navbar logo source has been updated to use the `pmb_replik.png` image from the assets folder instead of the previous URL.

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ url('/') }}">
-            <img src="https://www.uhb.ac.id/site/assets/files/2071/logo_uhb_r.420x0-is-pid1040.png" alt="Logo" width="30" height="28"
+            <img src="{{ asset('pmb_replik.png') }}" alt="Logo" width="30" height="28"
                 class="d-inline-block align-text-top">
             {{ config('app.name', 'Laravel') }}
         </a>
