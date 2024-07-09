a98a522b740fa6e04f0bcbed07787fb2287c9023
ipincamp <support@nur-arifin.my.id>
2024-07-09 22:39:54 +0700

    chore: Update bukti pembayaran table layout and add upload modal
    - Adjust the layout of the bukti pembayaran table to improve readability
    - Add a modal for uploading bukti pembayaran

--- resources/views/unggah-bukti-tf/index.blade.php
+++ resources/views/unggah-bukti-tf/index.blade.php
@@ -4,39 +4,43 @@
     <div class="container">
         <div class="row justify-content-center mt-3">
             <div class="col-md-10">
-                <div class="mb-2">
-                    <h1>Tabel yang berisi bukti pembayaran (upload foto bukti)</h1>
-                    <p>Desain by amal</p>
-                    <button class="btn btn-success">
+                <div class="mb-2 d-flex justify-content-between align-items-center">
+                    <h1>Bukti Pembayaran Daftar Ulang</h1>
+                    <button type="button" class="btn btn-success btn-square" data-bs-toggle="modal" data-bs-target="#modalUploadBukti">
                         <i class="fa fa-plus"></i> Upload Bukti
                     </button>
                 </div>
 
-                <table class=table>
-                    <thead>
-                        <tr>
-                            <th>Nama</th>
-                            <th>Bank</th>
-                            <th>Nominal</th>
-                            <th>Bukti</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($sampleData as $b)
-                        <tr>
-                            <td>{{ $b['nama'] }}</td>
-                            <td>{{ $b['bank'] }}</td>
-                            <td>{{ $b['nominal'] }}</td>
-                            <td>
-                                <a href="{{ asset('storage/bukti/' . $b['bukti']) }}" target="_blank">
-                                    <i class="fa fa-eye"></i> Lihat Bukti
-                                </a>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama</th>
+                                <th>Bank</th>
+                                <th>Nominal</th>
+                                <th>Bukti</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($sampleData as $b)
+                            <tr>
+                                <td style="width: 5%">{{ $loop->iteration }}</td>
+                                <td style="width: 25%">{{ $b['nama'] }}</td>
+                                <td style="width: 10%">{{ $b['bank'] }}</td>
+                                <td style="width: 20%">{{ $b['nominal'] }}</td>
+                                <td style="width: 10%">
+                                    <button class="btn btn-sm btn-primary" onclick="window.open('{{ asset('storage/bukti/' . $b['bukti']) }}', '_blank')">
+                                        <i class="fa fa-eye"></i> Lihat Bukti
+                                    </button>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
+    @include('unggah-bukti-tf.modal.upload-bukti')
 @endsection
